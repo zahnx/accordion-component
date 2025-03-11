1d05fdc3d2a9db103337fac125b9e7469c3267dc
zahnx <breadsw01@gmail.com>
2025-03-11 13:11:12 +0800

    Deleted unused file

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function AccordionItem({ num, title, text }) {
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{text}s</div>}
     </div>
   );
 }
